5119c05c59f6baf962ab254e07c3fce85cd7cb79
prjwl16 <prajumore76@gmail.com>
2023-09-06 20:43:32 +0530

    able to send messages from twilio api

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import './src/config/passport.js'
 import config from 'config'
 import { Middlewares } from './src/middleware/index.js'
+import { sendMessage } from './src/APIs/bots/twilioCalls.js'
 import { router } from './src/routes/index.js'
 
 //Initiate express app
@@ -14,6 +15,7 @@ Middlewares(app)
 
 //Test route
 app.get('/', async (_req, res) => {
+  await sendMessage('Hello', '919527189354')
   return res.send({ success: true, message: "hooray.... it's working bruh" })
 })
 
