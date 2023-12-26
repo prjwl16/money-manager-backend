75ea2c66b4af5667129e9b52db9655df820670c5
prjwl16 <prajumore76@gmail.com>
2023-12-27 01:42:40 +0530

    auto

--- prisma/client.js
+++ prisma/client.js
@@ -15,10 +15,5 @@ const prisma = new PrismaClient({
         },
     ],
 });
-prisma.$on('query', (e) => {
-    console.log('Query: ' + e);
-    console.log('Query: ' + e.query);
-    console.log('Params: ' + e.params);
-    console.log('Duration: ' + e.duration + 'ms');
-});
 export default prisma;
+//# sourceMappingURL=client.js.map
